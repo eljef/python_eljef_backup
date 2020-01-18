26b161f49156bac6dc2a2d8a2f5641dc1c551bae
Jef Oliver <jef@eljef.me>
2020-01-17 21:43:06 -0800

    Bump Version to 0.2.0
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=['eljef.backup', 'eljef.backup.cli', 'eljef.backup.plugins'],
     python_requires='>=3.7',
     url='https://github.com/eljef/python_eljef_backup',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
     entry_points={
         'console_scripts': [
