b917c7a0d1d226943b7300ed0b4837f555abde96
Jef Oliver <jef@eljef.me>
2020-02-01 15:58:40 -0800

    Rename and Version Bump
    * Rename eljef-backup to ej-backup
    * Bump version to 0.3.0
    
    Signed-off-by: Jef Oliver <jef@eljef.me>

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setup(
     packages=['eljef.backup', 'eljef.backup.cli', 'eljef.backup.plugins'],
     python_requires='>=3.7',
     url='https://github.com/eljef/python_eljef_backup',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
     entry_points={
         'console_scripts': [
-            'eljef-backup = eljef.backup.cli.__main__:main'
+            'ej-backup = eljef.backup.cli.__main__:main'
         ]
     },
 )
